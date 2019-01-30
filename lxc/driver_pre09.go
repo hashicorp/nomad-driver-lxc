54206a1902739953591680d59fc69ec12b87a18e
Mahmood Ali <mahmood@hashicorp.com>
2019-01-30 14:45:40 -0500

    Accept task ID for pre-0.9 tasks
    Port https://github.com/hashicorp/nomad/pull/5279 changes for LXC driver

--- lxc/driver_pre09.go
+++ lxc/driver_pre09.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/hashicorp/nomad/client/state"
 	"github.com/hashicorp/nomad/client/stats"
-	"github.com/hashicorp/nomad/helper/uuid"
 	"github.com/hashicorp/nomad/plugins/drivers"
 	lxc "gopkg.in/lxc/go-lxc.v2"
 )
@@ -15,8 +14,6 @@ func (d *Driver) recoverPre09Task(h *drivers.TaskHandle) error {
 	if err != nil {
 		return fmt.Errorf("failed to decode pre09 driver handle: %v", err)
 	}
-
-	h.Config.ID = fmt.Sprintf("pre09-%s", uuid.Generate())
 
 	c, err := lxc.NewContainer(handle.ContainerName, d.lxcPath())
 	if err != nil {
