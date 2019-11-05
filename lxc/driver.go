82c30d939b796303398f57c1b7341511a8e4796b
Thomas Weber <towe75@users.noreply.github.com>
2019-11-05 22:37:03 +0100

    Delete lxc container when task is destroyed (#12)
    * Introduce docker driver gc.container option; LXC Container is now really deleted when task is destroyed
    * Require go 1.12.x in travis

--- lxc/driver.go
+++ lxc/driver.go
@@ -47,6 +47,17 @@ var (
 			hclspec.NewLiteral("true"),
 		),
 		"lxc_path": hclspec.NewAttr("lxc_path", "string", false),
+		// garbage collection options
+		// default needed for both if the gc {...} block is not set and
+		// if the default fields are missing
+		"gc": hclspec.NewDefault(hclspec.NewBlock("gc", false, hclspec.NewObject(map[string]*hclspec.Spec{
+			"container": hclspec.NewDefault(
+				hclspec.NewAttr("container", "bool", false),
+				hclspec.NewLiteral("true"),
+			),
+		})), hclspec.NewLiteral(`{
+			container = true
+		}`)),
 	})
 
 	// taskConfigSpec is the hcl specification for the driver config section of
@@ -105,6 +116,11 @@ type Driver struct {
 	logger hclog.Logger
 }
 
+// GCConfig is the driver GarbageCollection configuration
+type GCConfig struct {
+	Container bool `codec:"container"`
+}
+
 // Config is the driver configuration set by the SetConfig RPC call
 type Config struct {
 	// Enabled is set to true to enable the lxc driver
@@ -113,6 +129,8 @@ type Config struct {
 	AllowVolumes bool `codec:"volumes_enabled"`
 
 	LXCPath string `codec:"lxc_path"`
+
+	GC GCConfig `codec:"gc"`
 }
 
 // TaskConfig is the driver configuration of a task within a job
@@ -445,7 +463,14 @@ func (d *Driver) DestroyTask(taskID string, force bool) error {
 			handle.logger.Error("failed to destroy executor", "err", err)
 		}
 	}
-
+	if d.config.GC.Container {
+		handle.logger.Debug("Destroying container", "container", handle.container.Name())
+		// delete the container itself
+		if err := handle.container.Destroy(); err != nil {
+			handle.logger.Error("failed to destroy lxc container", "err", err)
+		}
+	}
+	// finally cleanup task map
 	d.tasks.Delete(taskID)
 	return nil
 }
